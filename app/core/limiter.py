d78f97ea35abca8219ae4fe90d3f9e732356856d
f64116045 <f64116045@gs.ncku.edu.tw>
2026-02-08 08:30:42 +0800

    fix: Add BACKEND_CORS_ORIGIN in env example

--- app/core/limiter.py
+++ app/core/limiter.py
@@ -7,5 +7,5 @@ from app.core.config import settings
 limiter = Limiter(
     key_func=get_remote_address,
     storage_uri=f"redis://{settings.REDIS_HOST}:6379/0",
-    strategy="sliding-window",
+    strategy="fixed-window",
 )
